c4452da044c995fcf27b28b27f004873a4a8050c
Aurorien <starlightnoise@gmail.com>
2023-11-01 23:05:31 +0100

    Add and using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 import robot from "./assets/RobotAtMechanexus.png";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState<string | undefined>("empty");
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    axios
+      .get("https://robomechanexus-backend-test.onrender.com/")
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, [setData]);
 
   return (
     <>
@@ -14,6 +30,16 @@ function App() {
         </button>
       </div>
       <p>Robot mechatronics workshop</p>
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            <h1>API Response</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        )}
+      </div>
       <img src={robot} alt="Robot" />
     </>
   );
